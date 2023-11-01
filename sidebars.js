d8858d4ae2feabea65f782b5f389d43f95917bbd
Empiech008 <tonymmatlala@gmail.com>
2023-11-01 11:29:01 +0200

    Add zkevm bridge guide

--- sidebars.js
+++ sidebars.js
@@ -750,6 +750,7 @@ module.exports = {
             "zkevm/guides/using-foundry",
             "zkevm/guides/using-hardhat",
             "zkevm/guides/verify-smart-contract",
+            "zkevm/guides/zkevmbridge-guides",
           ],
         },
         {
